22ea2a807d0be800bca8a5f5b367fb1a9fcb1112
Paulo Black <pblack@goodwaygroup.com>
2022-04-21 07:24:10 -0400

    gopath etc

--- .languages.sh
+++ .languages.sh
@@ -9,3 +9,10 @@ export PATH="$HOME/.poetry/bin:$PATH"
 
 # Rust
 . "$HOME/.cargo/env"
+
+# Golang
+export GOPATH=$HOME/code/go
+export GOROOT=/usr/local/opt/go/libexec
+export GOBIN=$GOPATH/bin
+export PATH=$PATH:$GOPATH/bin
+export PATH=$PATH:$GOROOT/bin
